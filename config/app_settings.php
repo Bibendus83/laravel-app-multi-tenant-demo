713e6cb65d8f1619eaed58c3e167395a8eabf538
AndreaMauro@Home <bibendus1983@gmail.com>
2020-03-25 22:24:05 +0100

    fix

--- config/app_settings.php
+++ config/app_settings.php
@@ -92,8 +92,5 @@ return [
     'controller' => '\QCod\AppSettings\Controllers\AppSettingController',
 
     // settings group
-    'setting_group' => function() {
-        // return 'user_'.auth()->id();
-        return 'default';
-    }
+    'setting_group' => 'default',
 ];
