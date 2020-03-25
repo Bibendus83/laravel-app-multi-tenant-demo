91e9f747a88f388abb9b5ab14348198f9fa4c509
AndreaMauro@Home <bibendus1983@gmail.com>
2020-03-25 17:19:56 +0100

    fix to make users work in multi tenancy

--- app/User.php
+++ app/User.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
+use Hyn\Tenancy\Traits\UsesTenantConnection;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use UsesTenantConnection;
     use Notifiable;
 
     /**
