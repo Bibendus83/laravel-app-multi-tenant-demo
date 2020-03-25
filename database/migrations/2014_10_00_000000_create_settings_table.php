2992a231af3c174348ecf1cf322e62867a07f3e6
AndreaMauro@Home <bibendus1983@gmail.com>
2020-03-25 12:27:02 +0100

    installing and configuring qcod/laravel-app-settings

---
+++ database/migrations/2014_10_00_000000_create_settings_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Empty migrate to disable settings in system database
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
