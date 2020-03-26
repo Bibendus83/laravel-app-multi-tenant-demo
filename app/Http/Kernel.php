ee069f7565721b23987808ec79f15fee2086acc6
AndreaMauro@Home <bibendus1983@gmail.com>
2020-03-26 16:29:12 +0100

    disabled more external libraries

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrustProxies::class,
 
         // Custom middlewares
-        \App\Http\Middleware\ForceHttpsProtocol::class,
+        // \App\Http\Middleware\ForceHttpsProtocol::class,
     ];
 
     /**
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\InitJavascriptConstants::class,
+            // \App\Http\Middleware\InitJavascriptConstants::class,
         ],
 
         'api' => [
